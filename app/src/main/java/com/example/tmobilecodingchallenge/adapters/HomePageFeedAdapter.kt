83b8cd71bdff1669bdb02788add1335b5fb6e696
Sukhdip <=>
2021-09-25 17:49:54 -0700

    set up view holder to display data

--- app/src/main/java/com/example/tmobilecodingchallenge/adapters/HomePageFeedAdapter.kt
+++ app/src/main/java/com/example/tmobilecodingchallenge/adapters/HomePageFeedAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.tmobilecodingchallenge.adapters
 
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.tmobilecodingchallenge.databinding.ItemCardBinding
 import com.example.tmobilecodingchallenge.models.Cards
 
@@ -10,10 +14,47 @@ class HomePageFeedAdapter : RecyclerView.Adapter<HomePageFeedAdapter.ViewHolder>
 
     private var listOfCards = emptyList<Cards>()
 
-    class ViewHolder(private val itemCardBinding: ItemCardBinding) : RecyclerView.ViewHolder(itemCardBinding.root) {
-        fun bind(card: Cards) {
-            itemCardBinding.title.text = card.card.title?.value
-            itemCardBinding.description.text = card.card.description?.value
+    class ViewHolder(private val itemCardBinding: ItemCardBinding) :
+        RecyclerView.ViewHolder(itemCardBinding.root) {
+
+        fun bind(card: Cards, context: Context) {
+            when (card.cardType) {
+
+                "text" -> {
+                    itemCardBinding.title.text = card.card.value
+                    itemCardBinding.title.textSize = card.card.attributes!!.font.size.toFloat()
+                    itemCardBinding.title.setTextColor(Color.parseColor(card.card.attributes.textColor))
+
+                    itemCardBinding.description.visibility = View.GONE
+                }
+
+                "title_description" -> {
+                    itemCardBinding.title.text = card.card.title!!.value
+                    itemCardBinding.title.textSize = card.card.title.attributes.font.size.toFloat()
+                    itemCardBinding.title.setTextColor(Color.parseColor(card.card.title.attributes.textColor))
+
+                    itemCardBinding.description.text = card.card.description!!.value
+                    itemCardBinding.description.textSize =
+                        card.card.description.attributes.font.size.toFloat()
+                    itemCardBinding.description.setTextColor(Color.parseColor(card.card.description.attributes.textColor))
+                }
+
+                "image_title_description" -> {
+                    itemCardBinding.title.text = card.card.title!!.value
+                    itemCardBinding.title.textSize = card.card.title.attributes.font.size.toFloat()
+                    itemCardBinding.title.setTextColor(Color.parseColor(card.card.title.attributes.textColor))
+
+                    itemCardBinding.description.text = card.card.description!!.value
+                    itemCardBinding.description.textSize =
+                        card.card.description.attributes.font.size.toFloat()
+                    itemCardBinding.description.setTextColor(Color.parseColor(card.card.description.attributes.textColor))
+
+                    Glide.with(context)
+                        .load(card.card.image!!.url)
+                        .into(itemCardBinding.image)
+                }
+
+            }
         }
     }
 
@@ -25,7 +66,7 @@ class HomePageFeedAdapter : RecyclerView.Adapter<HomePageFeedAdapter.ViewHolder>
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val card = listOfCards[position]
-        holder.bind(card)
+        holder.bind(card, holder.itemView.context)
     }
 
     override fun getItemCount(): Int {
