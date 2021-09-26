413f6c6049fa419e9c135f66209e0d9b36cb13b0
Sukhdip <=>
2021-09-25 18:19:52 -0700

    clean up recycler view adapter

--- app/src/main/java/com/example/tmobilecodingchallenge/adapters/HomePageFeedAdapter.kt
+++ app/src/main/java/com/example/tmobilecodingchallenge/adapters/HomePageFeedAdapter.kt
@@ -1,6 +1,5 @@
 package com.example.tmobilecodingchallenge.adapters
 
-import android.content.Context
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -17,43 +16,68 @@ class HomePageFeedAdapter : RecyclerView.Adapter<HomePageFeedAdapter.ViewHolder>
     class ViewHolder(private val itemCardBinding: ItemCardBinding) :
         RecyclerView.ViewHolder(itemCardBinding.root) {
 
-        fun bind(card: Cards, context: Context) {
+        fun bind(card: Cards) {
+            handleCardTypeVisibility(card.cardType)
+
             when (card.cardType) {
-
                 "text" -> {
-                    itemCardBinding.title.text = card.card.value
-                    itemCardBinding.title.textSize = card.card.attributes!!.font.size.toFloat()
-                    itemCardBinding.title.setTextColor(Color.parseColor(card.card.attributes.textColor))
-
-                    itemCardBinding.description.visibility = View.GONE
+                    with(itemCardBinding.title) {
+                        text = card.card.value
+                        textSize = card.card.attributes!!.font.size.toFloat()
+                        setTextColor(Color.parseColor(card.card.attributes.textColor))
+                    }
                 }
 
                 "title_description" -> {
-                    itemCardBinding.title.text = card.card.title!!.value
-                    itemCardBinding.title.textSize = card.card.title.attributes.font.size.toFloat()
-                    itemCardBinding.title.setTextColor(Color.parseColor(card.card.title.attributes.textColor))
-
-                    itemCardBinding.description.text = card.card.description!!.value
-                    itemCardBinding.description.textSize =
-                        card.card.description.attributes.font.size.toFloat()
-                    itemCardBinding.description.setTextColor(Color.parseColor(card.card.description.attributes.textColor))
+                    with(itemCardBinding.title) {
+                        text = card.card.title!!.value
+                        textSize = card.card.title.attributes.font.size.toFloat()
+                        setTextColor(Color.parseColor(card.card.title.attributes.textColor))
+                    }
+                    with(itemCardBinding.description) {
+                        text = card.card.description!!.value
+                        textSize = card.card.description.attributes.font.size.toFloat()
+                        setTextColor(Color.parseColor(card.card.description.attributes.textColor))
+                    }
                 }
 
                 "image_title_description" -> {
-                    itemCardBinding.title.text = card.card.title!!.value
-                    itemCardBinding.title.textSize = card.card.title.attributes.font.size.toFloat()
-                    itemCardBinding.title.setTextColor(Color.parseColor(card.card.title.attributes.textColor))
+                    with(itemCardBinding.title) {
+                        text = card.card.title!!.value
+                        textSize = card.card.title.attributes.font.size.toFloat()
+                        setTextColor(Color.parseColor(card.card.title.attributes.textColor))
+                    }
+                    with(itemCardBinding.description) {
+                        text = card.card.description!!.value
+                        textSize = card.card.description.attributes.font.size.toFloat()
+                        setTextColor(Color.parseColor(card.card.description.attributes.textColor))
+                    }
 
-                    itemCardBinding.description.text = card.card.description!!.value
-                    itemCardBinding.description.textSize =
-                        card.card.description.attributes.font.size.toFloat()
-                    itemCardBinding.description.setTextColor(Color.parseColor(card.card.description.attributes.textColor))
-
-                    Glide.with(context)
+                    Glide.with(this.itemView.context)
                         .load(card.card.image!!.url)
                         .into(itemCardBinding.image)
                 }
 
+            }
+        }
+
+        private fun handleCardTypeVisibility(cardType: String) {
+            when (cardType) {
+                "text" -> {
+                    itemCardBinding.title.visibility = View.VISIBLE
+                    itemCardBinding.description.visibility = View.GONE
+                    itemCardBinding.image.visibility = View.GONE
+                }
+                "title_description" -> {
+                    itemCardBinding.title.visibility = View.VISIBLE
+                    itemCardBinding.description.visibility = View.VISIBLE
+                    itemCardBinding.image.visibility = View.GONE
+                }
+                "image_title_description" -> {
+                    itemCardBinding.title.visibility = View.VISIBLE
+                    itemCardBinding.description.visibility = View.VISIBLE
+                    itemCardBinding.image.visibility = View.VISIBLE
+                }
             }
         }
     }
@@ -66,7 +90,7 @@ class HomePageFeedAdapter : RecyclerView.Adapter<HomePageFeedAdapter.ViewHolder>
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val card = listOfCards[position]
-        holder.bind(card, holder.itemView.context)
+        holder.bind(card)
     }
 
     override fun getItemCount(): Int {
